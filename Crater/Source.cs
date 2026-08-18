8a376be1c3d9d25c30e157fc101196b8ba43804e
Periapsises <94928308+Periapsises@users.noreply.github.com>
2026-08-17 21:21:07 -0400

    Add source object

---
+++ Crater/Source.cs
@@ -0,0 +1,27 @@
+using Antlr4.Runtime;
+
+namespace Crater;
+
+public class Source(int startLine, int startColumn, int stopLine, int stopColumn)
+{
+    public readonly int StartLine = startLine;
+    public readonly int StartColumn = startColumn;
+    public readonly int StopLine = stopLine;
+    public readonly int StopColumn = stopColumn;
+
+    public bool isSingleLine => StartLine == StopLine;
+
+    public static Source FromToken(IToken token)
+    {
+        var startLine = token.Line;
+        var startColumn = token.Column;
+        var stopColumn = startColumn + token.Text.Length;
+
+        return new Source(startLine, startColumn, startLine, stopColumn);
+    }
+    
+    public static Source FromContext(ParserRuleContext context)
+    {
+        return new Source(context.Start.Line, context.Start.Column, context.Stop.Line, context.Stop.Column);
+    }
+}
