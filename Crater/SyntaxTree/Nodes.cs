9193cbbfd2762936e32c5a120b05e79a267c7b7a
Periapsises <94928308+Periapsises@users.noreply.github.com>
2026-08-18 22:33:16 -0400

    Update converter to support do statements

--- Crater/SyntaxTree/Nodes.cs
+++ Crater/SyntaxTree/Nodes.cs
@@ -2,6 +2,10 @@ namespace Crater.SyntaxTree;
 
 public abstract record Node(Source source);
 
-public sealed record Program(List<Node> nodes, Source source) : Node(source);
+public sealed record Program(Block block, Source source) : Node(source);
+
+public sealed record Block(List<Node> statements, Source source) : Node(source);
 
 public sealed record VariableDeclaration(bool local, string name, string type, Source source) : Node(source);
+
+public sealed record DoStatement(Block block, Source source) : Node(source);
--- Crater/SyntaxTree/SyntaxTreeConverter.cs
+++ Crater/SyntaxTree/SyntaxTreeConverter.cs
@@ -1,17 +1,33 @@
+using Antlr4.Runtime;
 using Crater.Antlr;
 
 namespace Crater.SyntaxTree;
 
 public class SyntaxTreeConverter : CraterParserBaseVisitor<Node>
 {
+    private T Get<T>(ParserRuleContext context)
+    {
+        var node = Visit(context);
+        if (node is T t)
+            return t;
+
+        throw new Exception($"Could not convert node to {typeof(T).Name} (was {node.GetType().Name})");
+    }
+    
     public override Node VisitProgram(CraterParser.ProgramContext context)
     {
-        var variableDeclarations = new List<Node>();
-        
-        foreach (var variableDeclaration in context.variableDeclaration())
-            variableDeclarations.Add(Visit(variableDeclaration));
+        var block = Get<Block>(context.block());
+        return new Program(block, Source.FromContext(context));
+    }
 
-        return new Program(variableDeclarations, Source.FromContext(context));
+    public override Node VisitBlock(CraterParser.BlockContext context)
+    {
+        var nodes = new List<Node>();
+        
+        foreach (var variableDeclaration in context.statement())
+            nodes.Add(Visit(variableDeclaration));
+
+        return new Block(nodes, Source.FromContext(context));
     }
 
     public override Node VisitVariableDeclaration(CraterParser.VariableDeclarationContext context)
@@ -21,5 +37,11 @@ public class SyntaxTreeConverter : CraterParserBaseVisitor<Node>
         var type = context.type.Text;
 
         return new VariableDeclaration(local, name, type, Source.FromContext(context));
+    }
+    
+    public override Node VisitDoStatement(CraterParser.DoStatementContext context)
+    {
+        var block = Get<Block>(context.block());
+        return new DoStatement(block, Source.FromContext(context));
     }
 }
