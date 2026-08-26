b58e829550a32a86e410f27badd0354e9c6430c6
Periapsises <94928308+Periapsises@users.noreply.github.com>
2026-08-25 23:27:43 -0400

    Add tests for array types

---
+++ Tests/ArrayTypeTests.cs
@@ -0,0 +1,38 @@
+using Crater.SemanticAnalysis;
+using Crater.SemanticAnalysis.Types;
+using Type = System.Type;
+
+namespace Tests;
+
+[TestFixture]
+public class ArrayTypeTests
+{
+    [Test]
+    public void CanHoldArrayOfSameType()
+    {
+        var arrayA = new ArrayType(TypeRegistry.StringType, TypeRegistry.AnyType);
+        var arrayB = new ArrayType(TypeRegistry.StringType, TypeRegistry.AnyType);
+
+        Assert.That(arrayA.CanHold(arrayB));
+    }
+
+    [Test]
+    public void IsInvariant()
+    {
+        var anyArray = new ArrayType(TypeRegistry.AnyType, TypeRegistry.AnyType);
+        var numberArray = new ArrayType(TypeRegistry.NumberType, TypeRegistry.AnyType);
+        var nullableNumberArray = new ArrayType(new NullableType(TypeRegistry.NumberType), TypeRegistry.AnyType);
+        var nullableStringArray = new ArrayType(new NullableType(TypeRegistry.StringType), TypeRegistry.AnyType);
+
+        var nullableNumberArray2 = new ArrayType(new NullableType(TypeRegistry.NumberType), TypeRegistry.AnyType);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(anyArray.CanHold(numberArray), Is.False);
+            Assert.That(nullableNumberArray.CanHold(numberArray), Is.False);
+            Assert.That(nullableNumberArray.CanHold(nullableStringArray), Is.False);
+
+            Assert.That(nullableNumberArray.CanHold(nullableNumberArray2));
+        });
+    }
+}
